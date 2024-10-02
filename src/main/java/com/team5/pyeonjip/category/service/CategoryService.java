2f7450ed957e9a5ad62df1efea640d29881eda0b
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-02 21:04:05 +0900

    Feat: 카테고리 기본 클래스 생성

---
+++ src/main/java/com/team5/pyeonjip/category/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.team5.pyeonjip.category.service;
+
+import com.team5.pyeonjip.category.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+}
