2190d629b79357191d833d051486a9e3df56a06f
jsjhyun <gusrnl98@naver.com>
2024-10-03 22:09:20 +0900

    Feat : entity 구현

--- src/main/java/com/team5/pyeonjip/order/entity/Order.java
+++ src/main/java/com/team5/pyeonjip/order/entity/Order.java
@@ -1,6 +1,7 @@
 package com.team5.pyeonjip.order.entity;
 
 import com.team5.pyeonjip.global.entity.BaseTimeEntity;
+import com.team5.pyeonjip.order.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,11 +10,27 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "orders") // 예약어 order 대체
 public class Order extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "recipient", nullable = false)
+    private String recipient; // 수령인
 
+    @Column(name = "phone_num", nullable = false)
+    private String phoneNum; // 전화번호
+
+    @Column(name = "requirement")
+    private String requirement; // 주문 시 요청사항
+
+    @Column(name = "total_price", nullable = false)
+    private Long totalPrice; // 총금액
+
+    @Column(name = "delivery_price", nullable = false)
+    private Integer deliveryPrice; // 배송비
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status; // 주문 상태
 }
--- src/main/java/com/team5/pyeonjip/order/entity/OrderDetail.java
+++ src/main/java/com/team5/pyeonjip/order/entity/OrderDetail.java
@@ -1,10 +1,7 @@
 package com.team5.pyeonjip.order.entity;
 
 import com.team5.pyeonjip.global.entity.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,4 +13,16 @@ public class OrderDetail extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity; // 수량
+
+    @Column(name = "product_name", nullable = false)
+    private String productName; // 상품 명
+
+    @Column(name = "product_price", nullable = false)
+    private String productPrice; // 상품 가격
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl; // 상품 이미지 경로
 }
