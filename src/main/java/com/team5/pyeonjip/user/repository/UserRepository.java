7a6fa4cbcfb53725896aca6275710c0832fb3e54
thoon <131009655+hthoon@users.noreply.github.com>
2024-10-03 20:02:56 +0900

    Feat: Security 단일토큰 JWT 회원가입 및 로그인 구현 중. 로그인 필터 등록 확인

--- src/main/java/com/team5/pyeonjip/user/repository/UserRepository.java
+++ src/main/java/com/team5/pyeonjip/user/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.team5.pyeonjip.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email);
 }
