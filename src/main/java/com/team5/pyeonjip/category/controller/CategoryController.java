a8ab42f81d5fdd75fadc093baa748e6cf891599e
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-04 23:23:33 +0900

    Feat : 카테고리 GET API 로직 구현

--- src/main/java/com/team5/pyeonjip/category/controller/CategoryController.java
+++ src/main/java/com/team5/pyeonjip/category/controller/CategoryController.java
@@ -1,10 +1,16 @@
 package com.team5.pyeonjip.category.controller;
 
 
+import com.team5.pyeonjip.category.dto.CategoryResponse;
 import com.team5.pyeonjip.category.service.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -12,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getCategories() {
+
+        List<CategoryResponse> categories = categoryService.getCategories();
+
+        return ResponseEntity
+                .status(categories.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK)  // 상태 코드 설정
+                .body(categories);
+    }
 }
--- src/main/java/com/team5/pyeonjip/category/service/CategoryService.java
+++ src/main/java/com/team5/pyeonjip/category/service/CategoryService.java
@@ -1,10 +1,24 @@
 package com.team5.pyeonjip.category.service;
 
+import com.team5.pyeonjip.category.dto.CategoryResponse;
+import com.team5.pyeonjip.category.entity.Category;
+import com.team5.pyeonjip.category.mapper.CategoryMapper;
 import com.team5.pyeonjip.category.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
 
     private CategoryRepository categoryRepository;
+    private CategoryMapper categoryMapper;
+
+    public List<CategoryResponse> getCategories() {
+        List<Category> rootCategories = categoryRepository.findByParentIsNull();
+
+        return rootCategories.stream()
+                .map(categoryMapper::toResponse)
+                .toList();
+    }
 }
