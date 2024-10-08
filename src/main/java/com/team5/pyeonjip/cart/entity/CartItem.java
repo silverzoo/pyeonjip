b14c0d99b6498b60eaf6a5dd553ddd0592ede185
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-10-08 21:58:23 +0900

    Feat/Cart Service 로직

--- src/main/java/com/team5/pyeonjip/cart/entity/CartItem.java
+++ src/main/java/com/team5/pyeonjip/cart/entity/CartItem.java
@@ -1,5 +1,6 @@
 package com.team5.pyeonjip.cart.entity;
 
+import com.team5.pyeonjip.product.entity.Product;
 import com.team5.pyeonjip.product.entity.ProductDetail;
 import com.team5.pyeonjip.product.entity.ProductImage;
 import jakarta.persistence.*;
@@ -25,10 +26,6 @@ public class CartItem {
     private Cart cart;
 
     @ManyToOne
-    @JoinColumn(name = "product_detail_id", nullable = false)
-    private ProductDetail productDetail;
-
-    @ManyToOne
-    @JoinColumn(name = "product_image_id", nullable = false)
-    private ProductImage productImage;
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product; // Product와의 연결
 }
