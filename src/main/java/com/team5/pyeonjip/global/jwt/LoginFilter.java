8a4afe80bc1864f3d3933f81e30af81f2a29f931
[CLOUD4] 현태훈 <winwin_th@naver.com>
2024-10-08 11:32:20 +0900

    Fix: 도메인별 테스트 위해 단일토큰 JWT 로그인으로 원상복구

--- src/main/java/com/team5/pyeonjip/global/jwt/LoginFilter.java
+++ src/main/java/com/team5/pyeonjip/global/jwt/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.team5.pyeonjip.global.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team5.pyeonjip.user.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -45,42 +46,42 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     // 단일 토큰 발급 방식에서 로그인 성공 시 사용했던 메서드
-//    @Override
-//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-//
-//        // 특정 유저를 확인
-//        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-//
-//        String email = customUserDetails.getUsername();
-//
-//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-//        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-//        GrantedAuthority auth = iterator.next();
-//
-//        String role = auth.getAuthority();
-//
-//        // Jwt 생성
-//        String token = jwtUtil.createJwt(email, role, 60 * 60 * 5L * 1000);
-//
-//        // Jwt를 Header에 담아 응답
-//        // Bearer 인증방식
-//        response.addHeader("Authorization", "Bearer " + token);
-//
-//        response.setStatus(200);
-//    }
-
-
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
 
-        // 토큰에 유저 정보와 role을 stream으로 받아온다.
-        String email = authentication.getName();
+        // 특정 유저를 확인
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String email = customUserDetails.getUsername();
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
-        GrantedAuthority auth = iter.next();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
         String role = auth.getAuthority();
+
+        // Jwt 생성
+        String token = jwtUtil.createJwt(email, role, 60 * 60 * 5L * 1000);
+
+        // Jwt를 Header에 담아 응답
+        // Bearer 인증방식
+        response.addHeader("Authorization", "Bearer " + token);
+
+        response.setStatus(200);
     }
+
+
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+//
+//        // 토큰에 유저 정보와 role을 stream으로 받아온다.
+//        String email = authentication.getName();
+//
+//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+//        GrantedAuthority auth = iter.next();
+//        String role = auth.getAuthority();
+//    }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
