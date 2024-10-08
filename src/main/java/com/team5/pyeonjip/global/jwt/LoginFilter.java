306bf83ddbf3208b4b1445cbf97c2b2ce2ec58eb
[CLOUD4] 현태훈 <winwin_th@naver.com>
2024-10-08 14:39:36 +0900

    Feat: 로그인 시 access(Header) & refresh(Cookie) 토큰 발급.

--- src/main/java/com/team5/pyeonjip/global/jwt/LoginFilter.java
+++ src/main/java/com/team5/pyeonjip/global/jwt/LoginFilter.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team5.pyeonjip.user.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -46,42 +48,52 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     // 단일 토큰 발급 방식에서 로그인 성공 시 사용했던 메서드
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-
-        // 특정 유저를 확인
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        String email = customUserDetails.getUsername();
-
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-        GrantedAuthority auth = iterator.next();
-
-        String role = auth.getAuthority();
-
-        // Jwt 생성
-        String token = jwtUtil.createJwt(email, role, 60 * 60 * 5L * 1000);
-
-        // Jwt를 Header에 담아 응답
-        // Bearer 인증방식
-        response.addHeader("Authorization", "Bearer " + token);
-
-        response.setStatus(200);
-    }
-
-
 //    @Override
 //    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
 //
-//        // 토큰에 유저 정보와 role을 stream으로 받아온다.
-//        String email = authentication.getName();
+//        // 특정 유저를 확인
+//        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+//
+//        String email = customUserDetails.getUsername();
 //
 //        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-//        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
-//        GrantedAuthority auth = iter.next();
+//        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+//        GrantedAuthority auth = iterator.next();
+//
 //        String role = auth.getAuthority();
+//
+//        // Jwt 생성
+//        String token = jwtUtil.createJwt(email, role, 60 * 60 * 5L * 1000);
+//
+//        // Jwt를 Header에 담아 응답
+//        // Bearer 인증방식
+//        response.addHeader("Authorization", "Bearer " + token);
+//
+//        response.setStatus(200);
 //    }
+
+
+    // 로그인 시 access & refresh 토큰 발급
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+
+        // 토큰에 유저 정보와 role을 stream으로 받아온다.
+        String email = authentication.getName();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        // access & refresh 토큰 생성
+        String access = jwtUtil.createJwt("access", email, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", email, role, 86400000L);
+
+        // 응답 설정
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
+    }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
@@ -102,5 +114,24 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         public String getPassword() {
             return password;
         }
+    }
+
+
+    // value에는 JWT가 들어감.
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60);
+
+        // https 통신 시
+        // cookie.setSecure(true);
+
+        // 쿠키가 적용될 범위
+        // cookie.setPath("/");
+
+        // js 등에서 쿠키에 접근하지 못하도록.
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
