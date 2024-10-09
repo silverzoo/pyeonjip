3db9da99687ab9b5cde5f4308d41b4de6ef42e01
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-09 21:31:41 +0900

    feat : 채팅 메시지 리스트 조회를 위한 비즈니스 로직 추가

--- src/main/java/com/team5/pyeonjip/chat/service/ChatMessageService.java
+++ src/main/java/com/team5/pyeonjip/chat/service/ChatMessageService.java
@@ -11,12 +11,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
     private final ChatMessageMapper chatMessageMapper;
+
+    public List<ChatMessageDto> getChatMessagesByChatRoomId(Long chatRoomId){
+        List<ChatMessage> chatMessages = chatMessageRepository.findByChatRoomId(chatRoomId);
+        List<ChatMessageDto> chatMessageDtos = new ArrayList<>();
+
+        for (ChatMessage chatMessage : chatMessages) {
+            chatMessageDtos.add(chatMessageMapper.toDTO(chatMessage));
+        }
+
+        return chatMessageDtos;
+    }
 
     public ChatMessageDto sendMessage(Long chatRoomId, String message){
         ChatMessage chatMessage = ChatMessage.builder()
@@ -40,5 +54,12 @@ public class ChatMessageService {
         chatMessageRepository.save(chatMessage);
 
         return chatMessageMapper.toDTO(chatMessage);
+    }
+
+    public void deleteMessage(Long messageId){
+        ChatMessage chatMessage = chatMessageRepository.findById(messageId)
+                .orElseThrow(() -> new ResourceNotFoundException("메시지를 찾을 수 없습니다."));
+
+        chatMessageRepository.delete(chatMessage);
     }
 }
