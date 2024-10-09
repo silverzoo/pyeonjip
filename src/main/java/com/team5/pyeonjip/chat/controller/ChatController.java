f4e8462d804f7351539368af11b1206fedcdb8fe
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-09 17:18:14 +0900

    Feat : 메시지 관련 엔드포인트 정의

--- src/main/java/com/team5/pyeonjip/chat/controller/ChatController.java
+++ src/main/java/com/team5/pyeonjip/chat/controller/ChatController.java
@@ -1,9 +1,12 @@
 package com.team5.pyeonjip.chat.controller;
 
+import com.team5.pyeonjip.chat.dto.ChatMessageDto;
 import com.team5.pyeonjip.chat.dto.ChatRoomDto;
+import com.team5.pyeonjip.chat.entity.ChatMessage;
 import com.team5.pyeonjip.chat.entity.ChatRoom;
 import com.team5.pyeonjip.chat.service.ChatMessageService;
 import com.team5.pyeonjip.chat.service.ChatRoomService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +23,7 @@ public class ChatController {
     private final ChatMessageService chatMessageService;
 
     // userId에 따른 채팅이력 리스트
-    @GetMapping("/chat_room/{userId}")
+    @GetMapping("/chat_room_List/{userId}")
     public ResponseEntity<List<ChatRoomDto>> getChatRooms(@PathVariable("userId") Long userId){
         List<ChatRoomDto> chatRoom = chatRoomService.getChatRoomsByUserId(userId);
         return ResponseEntity.ok().body(chatRoom);
@@ -29,14 +32,24 @@ public class ChatController {
     // 채팅방 생성
     @PostMapping("/chat_room")
     public ResponseEntity<ChatRoomDto> createChatRoom(@RequestBody ChatRoomDto chatRoomDto){
-        ChatRoomDto createChatRoom = chatRoomService.createChatRoom(chatRoomDto);
-        return ResponseEntity.ok().body(createChatRoom);
+        ChatRoomDto createdChatRoom = chatRoomService.createChatRoom(chatRoomDto);
+        return ResponseEntity.ok().body(createdChatRoom);
     }
+
 
     // 메시지 보내기
     @PostMapping("/message")
-    public ResponseEntity<Void> sendMessage(@RequestBody String message, @RequestParam("chatRoomId") Long chatRoomId){
-        chatMessageService.sendMessage(chatRoomId, message);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ChatMessageDto> sendMessage( @RequestParam("chatRoomId") Long chatRoomId, @RequestBody String message){
+        ChatMessageDto createdMessage = chatMessageService.sendMessage(chatRoomId, message);
+
+        return ResponseEntity.ok().body(createdMessage);
+    }
+
+    // 메시지 수정
+    @PutMapping("/message/{messageId}")
+    public ResponseEntity<ChatMessageDto> modifyMessage(@PathVariable("messageId") Long messageId, @RequestBody String message){
+        ChatMessageDto modifiedMessage = chatMessageService.modifyMessage(messageId, message);
+        System.out.println(modifiedMessage.toString());
+        return ResponseEntity.ok().body(modifiedMessage);
     }
 }
