37102fdb38a15f28a75af10c0f2f8c9d9cc4edfe
jsjhyun <gusrnl98@naver.com>
2024-10-09 20:51:27 +0900

    fix: 필요한 변수 추가

--- src/main/java/com/team5/pyeonjip/order/dto/OrderResponseDto.java
+++ src/main/java/com/team5/pyeonjip/order/dto/OrderResponseDto.java
@@ -1,9 +1,12 @@
 package com.team5.pyeonjip.order.dto;
 
+import com.team5.pyeonjip.order.entity.Order;
+import com.team5.pyeonjip.order.enums.DeliveryStatus;
 import com.team5.pyeonjip.order.enums.OrderStatus;
 import com.team5.pyeonjip.user.entity.Grade;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 // 관리자 - 주문 응답
@@ -13,8 +16,18 @@ import java.util.List;
 @Builder
 public class OrderResponseDto {
     private Long id; // 주문 id
-    private String recipient; // 구매자
+    private String userName; // 구매자
     private String phoneNumber; // 연락처
-    private OrderStatus status; // 기본 ORDER
-    private List<OrderDetailDto> orderDetails; // 상품 명, 상품 수량, 이미지
+    private OrderStatus orderStatus; // 기본 ORDER
+    private Long totalPrice;
+    private LocalDateTime createdAt;
+    private DeliveryStatus deliveryStatus;
+    private List<OrderDetailDto> orderDetails; // 상품 명, 상품 수량
+
+    public static OrderResponseDto from(Order order) {
+        return new OrderResponseDto(order.getId(),
+                order.getUser().getName(), order.getUser().getPhoneNumber(), order.getStatus(),
+                order.getTotalPrice(), order.getCreatedAt().toLocalDateTime(), order.getDelivery().getStatus(),
+                order.getOrderDetails().stream().map(OrderDetailDto::from).toList());
+    }
 }
