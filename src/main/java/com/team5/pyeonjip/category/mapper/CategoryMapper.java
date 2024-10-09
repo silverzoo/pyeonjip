33ab833857f38d76be4a4ee0671f15f2933a017d
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-09 20:23:09 +0900

    Refactor : 카테고리 엔티티, DTO 필드 및 연관 매핑 수정

--- src/main/java/com/team5/pyeonjip/category/mapper/CategoryMapper.java
+++ src/main/java/com/team5/pyeonjip/category/mapper/CategoryMapper.java
@@ -9,9 +9,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
-    @Mapping(source = "parentId", target = "parent.id")
     Category toEntity(CategoryRequest request);
 
-    @Mapping(source = "parent.id", target = "parentId")
     CategoryResponse toResponse(Category category);
 }
