902c5dd546746327431b3cf0752b1b56b5ddecdb
jsjhyun <gusrnl98@naver.com>
2024-10-09 20:52:51 +0900

    feat: 전체 상품 조회, 상품 이름 별 조회, 주문 상세 정보 삭제, 배송 정보 수정 기능 추가

--- src/main/java/com/team5/pyeonjip/order/controller/AdminOrderApiController.java
+++ src/main/java/com/team5/pyeonjip/order/controller/AdminOrderApiController.java
@@ -1,9 +1,7 @@
 package com.team5.pyeonjip.order.controller;
 
 import com.team5.pyeonjip.order.dto.OrderResponseDto;
-import com.team5.pyeonjip.order.dto.OrderUpdateRequestDto;
-import com.team5.pyeonjip.order.entity.Order;
-import com.team5.pyeonjip.order.service.OrderService;
+import com.team5.pyeonjip.order.service.OrderAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,35 +13,37 @@ import java.util.List;
 @RequestMapping("/admin/api")
 public class AdminOrderApiController {
 
-    private final OrderService orderService;
+    private final OrderAdminService orderService;
 
     // 관리자 - 주문 전체 조회
     @GetMapping("/order/list")
-    public ResponseEntity<List<Order>> getAllOrders(){
+    public List<OrderResponseDto> getAllOrders(){
         // 전체 주문 목록 조회
-        List<Order> orderList = orderService.findAllOrders();
+        return orderService.findAllOrders();
+    }
 
-        return ResponseEntity.ok(orderList);
+    // 관리자 - 특정 주문 조회
+    @GetMapping("/order")
+    public List<OrderResponseDto> getOrdersByUserName(@RequestParam("userName") String userName) {
+        return orderService.findOrdersByUserName(userName);
     }
 
     // 관리자 - 주문 수정
-    @PutMapping("order/{orderId}")
-    public ResponseEntity<OrderResponseDto> updateDeliveryStatus(
+    @PatchMapping("order/{orderId}")
+    public ResponseEntity<Void> updateDeliveryStatus(
             @PathVariable("orderId") Long orderId,
-            @RequestBody OrderUpdateRequestDto orderUpdateRequestDto) {
+            @RequestParam("deliveryStatus") String deliveryStatus) {
 
         // 배송 상태 변경 처리
-        OrderResponseDto updatedOrder = orderService.updateOrder(orderId, orderUpdateRequestDto);
-
-        return ResponseEntity.ok(updatedOrder);
+        orderService.updateDeliveryStatus(orderId, deliveryStatus);
+        return ResponseEntity.ok().build();
     }
 
     // 관리자 - 주문 삭제
     @DeleteMapping("order/{orderId}")
     public ResponseEntity<Void> deleteOrder(@PathVariable("orderId") Long orderId) {
         // 주문 삭제 처리
-        orderService.deleteOrderById(orderId);
-
-        return ResponseEntity.noContent().build();
+        orderService.deleteOrderDetailById(orderId);
+        return ResponseEntity.ok().build();
     }
 }
