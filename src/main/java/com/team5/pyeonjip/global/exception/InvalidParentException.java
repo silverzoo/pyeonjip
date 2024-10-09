75135e887382fe844b2ea8fd373a59602e0e0591
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-09 20:24:02 +0900

    Feat : 유효하지않은 상위카테고리 접근 예외클래스 추가

---
+++ src/main/java/com/team5/pyeonjip/global/exception/InvalidParentException.java
@@ -0,0 +1,7 @@
+package com.team5.pyeonjip.global.exception;
+
+public class InvalidParentException extends RuntimeException {
+    public InvalidParentException(String message) {
+        super(message);
+    }
+}
