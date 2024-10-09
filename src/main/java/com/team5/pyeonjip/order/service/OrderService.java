c3885bc90caa872af717d88e74b21d6d28c8c6b6
jsjhyun <gusrnl98@naver.com>
2024-10-09 20:50:06 +0900

    fix: return type 변경

--- src/main/java/com/team5/pyeonjip/order/service/OrderService.java
+++ src/main/java/com/team5/pyeonjip/order/service/OrderService.java
@@ -2,27 +2,13 @@ package com.team5.pyeonjip.order.service;
 
 import com.team5.pyeonjip.order.dto.*;
 import com.team5.pyeonjip.order.entity.Order;
-import com.team5.pyeonjip.user.entity.User;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface OrderService {
 
     // 주문 생성
-    OrderResponseDto createOrder(OrderRequestDto orderRequestDto, User user);
-
-    // 사용자 이름으로 주문 내역 검색
-    List<Order> findAllOrdersByUserName(String userName);
-
-    // 주문 수정
-    OrderResponseDto updateOrder(Long id, OrderUpdateRequestDto requestDto);
-
-    // 주문 삭제
-    void deleteOrderById(Long id);
-
-    // 주문 전체 조회
-    List<Order> findAllOrders();
+    void createOrder(OrderRequestDto orderRequestDto, Long userId);
 
     // 사용자 id로 주문 전체 조회
     List<OrderResponseDto> findOrdersByUserId(Long userId);
@@ -31,5 +17,5 @@ public interface OrderService {
     Order findOrderDetail(Long id);
 
     // 주문 취소
-    OrderResponseDto cancelOrder(Long id);
+    void cancelOrder(Long orderId);
 }
