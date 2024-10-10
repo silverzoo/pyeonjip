9cc0382a756a9f49e2081fa3c6dc687141bdbdbb
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-10 12:56:47 +0900

    Feat : 미사용 예외처리 클래스 삭제

--- src/main/java/com/team5/pyeonjip/global/exception/InvalidParentException.java
+++
@@ -1,7 +0,0 @@
-package com.team5.pyeonjip.global.exception;
-
-public class InvalidParentException extends RuntimeException {
-    public InvalidParentException(String message) {
-        super(message);
-    }
-}
