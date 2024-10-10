9a7c8e8a2dc67891dd28a0cbf3615c947feb9369
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-10 23:26:31 +0900

    Style : 어드민카테고리 컨트롤러 이름 수정

--- src/main/java/com/team5/pyeonjip/category/controller/AdminCategoryController.java
+++ src/main/java/com/team5/pyeonjip/category/controller/AdminCategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/category")
-public class AdminCategoryApiController {
+public class AdminCategoryController {
 
     private final CategoryService categoryService;
 
