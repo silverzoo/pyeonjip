1bce64175c846b7e12eb2c464b078b0780094016
jsjhyun <gusrnl98@naver.com>
2024-10-12 12:43:26 +0900

    feat : 유효성 검사 추

--- src/main/java/com/team5/pyeonjip/order/dto/OrderRequestDto.java
+++ src/main/java/com/team5/pyeonjip/order/dto/OrderRequestDto.java
@@ -1,5 +1,6 @@
 package com.team5.pyeonjip.order.dto;
 
+import com.team5.pyeonjip.order.entity.Delivery;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -19,10 +20,10 @@ public class OrderRequestDto {
     @Size(min = 11, max = 11, message = "연락처는 '-' 없이 11자리이어야 합니다.")
     private String phoneNumber;
 
+    @NotBlank(message = "배송지는 필수 입력 항목입니다.")
+    private String address;
+
     private String requirement;
 
     private List<OrderDetailDto> orderDetails;
-
-    @NotBlank(message = "배송지는 필수 입력 항목입니다.")
-    private String address;
 }
