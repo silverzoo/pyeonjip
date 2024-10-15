50404b04156bde8fcd1bdaf31c18fc30e0bd258b
thoon <winwin_th@naver.com>
2024-10-15 10:29:40 +0900

    Fix: @RequestBody 어노테이션 추가. dto가 전달되지 않는 문제 해결

--- src/main/java/com/team5/pyeonjip/user/controller/SignUpController.java
+++ src/main/java/com/team5/pyeonjip/user/controller/SignUpController.java
@@ -5,6 +5,7 @@ import com.team5.pyeonjip.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
@@ -17,7 +18,7 @@ public class SignUpController {
 
     // Todo: 현재 항상 "ok"를 반환하도록 구성되어 있음. 추후 성공 / 실패 결과를 나누어 보여줄 것.
     @PostMapping("/signup")
-    public String signUpProcess(SignUpDto dto) {
+    public String signUpProcess(@RequestBody SignUpDto dto) {
 
         signUpService.signUpProcess(dto);
 
