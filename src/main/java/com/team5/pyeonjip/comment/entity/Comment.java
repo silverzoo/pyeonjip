5fe4da5daaadb08777fba990fabf03d536682a67
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-10-18 17:15:04 +0900

    Feat/Cart - Comment 도메인 생성

---
+++ src/main/java/com/team5/pyeonjip/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.team5.pyeonjip.comment.entity;
+
+
+import com.team5.pyeonjip.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private String email;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
